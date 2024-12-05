7c8cb1a625a0b7901ec5cd1aa349e73bba7d7b60
Avinashannand <avinashannand@gmail.com>
2024-12-05 10:36:05 +0530

    Java Hello World

---
+++ HelloWorld/src/HelloWorldDemo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HelloWorldDemo {
+
+	/**
+	 * 
+	 */
+	public HelloWorldDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World. Start coding and happy coding");
+
+	}
+
+}
